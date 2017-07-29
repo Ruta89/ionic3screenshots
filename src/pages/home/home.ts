d0e4415f4dc7f34b06bd000ecc87b83a9a66da07
Arek Ruta <arek.ruta89@gmail.com>
2017-07-29 20:12:30 +0200

    screenShot save screenShotURI, view image, add style

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Screenshot } from '@ionic-native/screenshot';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  screen: any;
+  state: boolean = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private screenshot: Screenshot) {}
 
+  reset() {
+    let self = this;
+    setTimeout(() => {
+      self.state = false;
+    }, 1000);
   }
 
+  screenShot() {
+    this.screenshot.save('jpg', 80, 'myscreenshot.jpg').then(res => {
+      this.screen = res.filePath;
+      this.state = true;
+      this.reset();
+    });
+  }
+
+  screenShotURI() {
+    this.screenshot.URI(80).then(res => {
+      this.screen = res.URI;
+      this.state = true;
+      this.reset();
+    });
+  }
 }
